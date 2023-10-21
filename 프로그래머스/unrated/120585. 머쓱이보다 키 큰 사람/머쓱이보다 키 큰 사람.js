0420e6e04110fc18075b363c2f0acb49b8db5c53
oh-yeongji <130665424+oh-yeongji@users.noreply.github.com>
2023-10-22 00:16:06 +0900

    [level unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,4 +1,9 @@
-const solution = (array, height) => {
-     return array.filter(tall => tall > height).length;
-
+const solution =(array , height ) => {
+    var result = 0;
+        for(i =0; i < array.length; i++) {
+            if (height < array[i] ){
+                result++;
+        }
+    }
+        return result;
 }
