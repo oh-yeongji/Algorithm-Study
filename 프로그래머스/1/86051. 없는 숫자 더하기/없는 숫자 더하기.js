e0a861b03393cf056aa40edb38d2bd789ff437df
oh-yeongji <130665424+oh-yeongji@users.noreply.github.com>
2023-12-15 00:14:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,12 @@
 const solution = (numbers) => {
+    let number = [ 0,1,2,3,4,5,6,7,8,9];
+    //let number = [...Array(10)];
     let result = 0;
-      for( i =0 ; i < 10; i++){
-          if(numbers.includes(i) === false) {
-              result += i;
-          }
-      }
-     return result;  
+    number.forEach((i)=>{
+        if(numbers.includes(i)===false){
+        result += i
+        }
+    })
+    return result;
+      
 }
