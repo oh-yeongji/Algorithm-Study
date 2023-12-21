672c29612da6024b9c2252eddf0f33d725dddecd
oh-yeongji <130665424+oh-yeongji@users.noreply.github.com>
2023-12-21 23:26:36 +0900

    [level 1] Title: 내적, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,13 +1,8 @@
-
 const solution = (a, b) => {
-    
     let result = 0;
-        if (a.length == b.length) {
-            for (let i = 0; i < a.length; i++) {
-                result += a[i] * b[i];
+    for( i = 0 ; i < a.length ; i++) {
+        result += a[i] * b[i];
     }
-    }
-
-   return result;
-   
+    return result;
 };
+   
