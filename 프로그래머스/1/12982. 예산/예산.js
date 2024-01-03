6e1db893b6f91be2d63daa91005a50d5ac39017d
oh-yeongji <130665424+oh-yeongji@users.noreply.github.com>
2024-01-03 22:52:05 +0900

    [level 1] Title: 예산, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,15 @@
+const solution = (d, budget) =>{
+    let sum = 0;
+    let result = 0;
+    
+    d.sort( (a,b) => a - b); 
+    for( i = 0; i < d.length; i++){
+        sum += d[i];
+        result++;   
+        
+        if(sum > budget){
+        result--;
+        }
+    }
+    return result;
+}
