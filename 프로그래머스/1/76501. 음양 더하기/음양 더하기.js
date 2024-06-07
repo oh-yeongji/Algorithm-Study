ec686614eeabcf6baf58b6d6ca92e6a910052ce1
oh-yeongji <130665424+oh-yeongji@users.noreply.github.com>
2024-06-07 22:28:36 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,15 +1,4 @@
-const solution = (absolutes, signs) => {
-    let result = 0;
-    for( i = 0 ; i < signs.length ; i++ ) {
-        if( signs[i]===true) {
-             result  += absolutes[i] ;
-        }
-        else {
-             result  -= absolutes[i];
-        }
+function solution(absolutes, signs) {
 
-    }
-    
-  return result;
+    return absolutes.reduce((acc, val, i) => acc + (val * (signs[i] ? 1 : -1)), 0);
 }
-
