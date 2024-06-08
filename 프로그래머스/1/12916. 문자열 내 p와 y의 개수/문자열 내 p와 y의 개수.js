af0a0686954a6652a103e39634d0c3cba139053c
oh-yeongji <130665424+oh-yeongji@users.noreply.github.com>
2024-06-08 23:26:43 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,5 +1,8 @@
-const solution = (s) => {
-    return ( s.match(/p/ig) || [] ).length === ( s.match(/y/ig) || [] ).length ?
-        true : false
+function solution(s){
 
-}  
+    return [...s.toLowerCase()].reduce((acc, cur) => {
+        if(cur ==='p') return acc + 1;
+        else if(cur ==='y') return acc - 1;
+        return acc;
+    }, 0) ? false : true;
+}
