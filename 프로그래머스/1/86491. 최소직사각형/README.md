716439f3d6745e8a1edce3ad46023c0b50bfc38b
oh-yeongji <130665424+oh-yeongji@users.noreply.github.com>
2024-06-17 23:35:14 +0900

    [level 1] Title: 최소직사각형, Time: 3.65 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=javascript) 
 
 ### 성능 요약
 
-메모리: 35.8 MB, 시간: 1.39 ms
+메모리: 39.8 MB, 시간: 3.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 23:8:36
+2024년 06월 17일 23:35:11
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,6 +1,4 @@
-const solution = (sizes) => {
-   const maxW = Math.max(...sizes.map(card => Math.max(...card)));
-     const maxH = Math.max(...sizes.map(card => Math.min(...card)));
-    return maxW * maxH;
-   
+function solution(sizes) {
+    const [hor, ver] = sizes.reduce(([h, v], [a, b]) => [Math.max(h, Math.max(a, b)), Math.max(v, Math.min(a, b))], [0, 0])
+    return hor * ver;
 }
