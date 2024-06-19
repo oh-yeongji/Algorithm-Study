0e9569b2ff1f967e1250b16bc9bfb0d84f93d7b1
oh-yeongji <130665424+oh-yeongji@users.noreply.github.com>
2024-06-19 23:24:05 +0900

    [level 1] Title: 삼총사, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/README.md
+++ 프로그래머스/1/131705. 삼총사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 삼총사 - 131705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.15 ms
+메모리: 33.5 MB, 시간: 0.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 20:28:12
+2024년 06월 19일 23:24:01
 
 ### 문제 설명
 
--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,14 +1,16 @@
-const solution = (number) => {
-    let count = 0;
-     for( i = 0 ; i < number.length - 2; i++ ){
-         for( j = i + 1; j < number.length - 1; j++ ){
-             for( k = j + 1; k < number.length; k++ ) {
-                if(number[i] + number[j] + number[k] == 0 ) {
-                    count += 1;
-             }else  0;
-             }
-         }
+function solution(number) {
+    let result = 0;
+
+    const combination = (current, start) => {
+        if (current.length === 3) {
+            result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+            return;
         }
-    return count;
-    
+
+        for (let i = start; i < number.length; i++) {
+            combination([...current, number[i]], i + 1);
+        }
+    }
+    combination([], 0);
+    return result;
 }
