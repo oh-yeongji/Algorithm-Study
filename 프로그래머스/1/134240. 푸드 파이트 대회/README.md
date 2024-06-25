136e1b7b0ea3e67935641731dfde870d1687694b
oh-yeongji <130665424+oh-yeongji@users.noreply.github.com>
2024-06-25 22:42:46 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/README.md
+++ 프로그래머스/1/134240. 푸드 파이트 대회/README.md
@@ -1,10 +1,10 @@
 # [level 1] 푸드 파이트 대회 - 134240 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.17 ms
+메모리: 33.5 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 23:32:5
+2024년 06월 25일 22:42:43
 
 ### 문제 설명
 
--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,19 +1,8 @@
 function solution(food) {
-    let answer = '';
-    const answerfoods = []
-    const eachFood = food.map(i => parseInt(i / 2))
+    let res = '';
+    for (let i = 1; i < food.length; i++) {
+        res += String(i).repeat(Math.floor(food[i]/2));
+    }
 
-    const foods = eachFood.reduce((acc, cur, ind, arr) => {
-        const tempFoods = []
-        for (let i = 1; i <= cur; i++) {
-            tempFoods.push(ind)
-        }
-        acc.push(...tempFoods)
-        return acc
-    }, [])
-
-    answerfoods.push(...foods, 0, ...foods.reverse())
-    answer = answerfoods.join("")
-
-    return answer;
+    return res + '0' + [...res].reverse().join('');
 }
